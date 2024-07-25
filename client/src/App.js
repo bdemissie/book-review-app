cfe5fd86da57a51d2cba185d970ea5436ad1332e
bdemissie <bemnet.hdemissie@gmail.com>
2024-07-24 21:02:59 -0400

    project structure complete

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+// client/src/App.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
