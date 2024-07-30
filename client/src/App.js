e3825a7a059fd2cedad562bd5cc5f136e21c1ff8
bdemissie <bemnet.hdemissie@gmail.com>
2024-07-30 18:46:59 -0400

    Routers added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import HomePage from './layouts/Homepage/HomePage'
-import SignInPage from './layouts/SignInSignUpPage/SignInPage'
-import BookReviewPage from './layouts/BookReviewPage/BookReviewPage'
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import NavBar from './layouts/NavBarAndFooter/NavBar'
 import Footer from './layouts/NavBarAndFooter/Footer'
+import HomePage from './layouts/Homepage/HomePage'
+import BookReviewPage from './layouts/BookReviewPage/BookReviewPage'
+import SignInPage from './layouts/SignInSignUpPage/SignInPage'
+
 
 function App() {
 
   return (
-    
-    <>
-    <NavBar />
-    <HomePage />
-    <SignInPage />
-    <BookReviewPage />
-    <Footer />
-    </>
+
+    <div className='d-flex flex-column min-vh-100'>
+      <NavBar />
+      <div className='flex-grow-1'>
+      <Routes>
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/sign-in' element={<SignInPage />} />
+        <Route path='/bookreview' element={<BookReviewPage />} />
+      </Routes>
+      </div>
+      <Footer />
+    </div>
 
   )
 }
